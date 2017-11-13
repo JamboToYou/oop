72cc998c7afe18c8264f77ee195630a91e34d8a8
Андрей <31936190+JamboToYou@users.noreply.github.com>
2017-11-13 23:58:03 +0200

    1

--- Practics_3/Practics_3/Practics_3/Program.cs
+++ Practics_3/Practics_3/Practics_3/Program.cs
@@ -10,6 +10,7 @@ namespace Practics_3
     {
         static void Main(string[] args)
         {
+            ICollection
         }
     }
 }
---
+++ Practics_4/Practics_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practics_4
+{
+    class Program
+    {
+        public delegate int func(int x, int y);
+
+        public static int submit(int x, int y)
+        {
+            return x + y;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
