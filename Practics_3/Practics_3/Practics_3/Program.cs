2c39630fc3a486e3be10b7e76de3270942556b41
Андрей <31936190+JamboToYou@users.noreply.github.com>
2017-12-18 13:33:29 +0400

    1

--- Practics_3/Practics_3/Practics_3/Program.cs
+++ Practics_3/Practics_3/Practics_3/Program.cs
@@ -12,6 +12,7 @@ namespace Practics_3
         {
             LinkedList<Human> list = new LinkedList<Human>();
             LinkedListNode<Human> node;
+            LinkedListNode<Human> nodeToDelete;
 
             Console.WriteLine("Enter the count of people :");
             if (!int.TryParse(Console.ReadLine(), out int cnt)) throw new ArgumentException("Positive decimal number required");
@@ -25,7 +26,7 @@ namespace Practics_3
 
             for (int i = 0; i < cnt; i++)
             {
-                Console.WriteLine(node.Value.getId());
+                Console.WriteLine("Human in the ring :" + node.Value.getId());
                 node = node.Next;
             }
 
@@ -35,16 +36,25 @@ namespace Practics_3
 
             node = list.First;
 
-            for (int i = 0; i < cnt; i++)
+            for (int i = 0; i < cnt - 1; i++)
             {
                 node = node.Next ?? list.First;
-
-                Console.WriteLine(node.Value.getId());
+                PrintEliminatedHuman(node);
+                nodeToDelete = node;
 
                 node = node.Next ?? list.First;
+
+                list.Remove(nodeToDelete);
             }
 
+            Console.WriteLine("The man #" + list.First.Value.getId() + " is the Last Hero!");
+
             Console.ReadKey();
+        }
+
+        private static void PrintEliminatedHuman(LinkedListNode<Human> node)
+        {
+            Console.WriteLine("Human #" + node.Value.getId() + " has eliminated");
         }
     }
 }
